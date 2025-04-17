b16b1e08690dbc4f7232ae082391f7647e1f1186
jeongseung-won <jsw00415@naver.com>
2025-04-17 14:36:50 +0900

    // Lv.2 fetch join으로 구현 되었던 부분을 @EntityGraph 버전으로 수정하여 똑같이 N+1문제를 해결하고자 함

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -3,6 +3,7 @@ package org.example.expert.domain.todo.repository;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,9 +15,14 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN FETCH t.user " +
-            "WHERE t.id = :todoId")
+    //Lv.2 jpql의 페치조인을 통해 todo와 user를 조인해 N+1을 해결중
+//    @Query("SELECT t FROM Todo t " +
+//            "LEFT JOIN FETCH t.user " +
+//            "WHERE t.id = :todoId")
+
+    // Lv.2 @EntityGraph를 통해 같은 동작을 수행
+    @EntityGraph(attributePaths = "user")
+    @Query("SELECT t FROM Todo t WHERE t.id = :todoId")
     Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
 
     int countById(Long todoId);
