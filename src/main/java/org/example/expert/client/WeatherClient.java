ce85e3a11262a60a2e07d4bc0f61396d7c46b590
jeongseung-won <jsw00415@naver.com>
2025-04-17 13:58:47 +0900

    // Lv.1-2 불필요한 if-else문 제거 어차피 예외를 던지면 그 다음에 작성되어있는 코드들은 조건이 맞지않아야 실행됨

--- src/main/java/org/example/expert/client/WeatherClient.java
+++ src/main/java/org/example/expert/client/WeatherClient.java
@@ -26,14 +26,15 @@ public class WeatherClient {
         ResponseEntity<WeatherDto[]> responseEntity =
                 restTemplate.getForEntity(buildWeatherApiUri(), WeatherDto[].class);
 
+        // Lv.1-2 불필요한 if-else문 제거
         WeatherDto[] weatherArray = responseEntity.getBody();
         if (!HttpStatus.OK.equals(responseEntity.getStatusCode())) {
             throw new ServerException("날씨 데이터를 가져오는데 실패했습니다. 상태 코드: " + responseEntity.getStatusCode());
-        } else {
-            if (weatherArray == null || weatherArray.length == 0) {
-                throw new ServerException("날씨 데이터가 없습니다.");
-            }
         }
+        if (weatherArray == null || weatherArray.length == 0) {
+                throw new ServerException("날씨 데이터가 없습니다.");
+        }
+
 
         String today = getCurrentDate();
 
